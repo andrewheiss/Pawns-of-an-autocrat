6a7de5cc7b7dff981007936266fa5f3f5c5d420e
Andrew Heiss <andrew@andrewheiss.com>
2015-02-06 23:14:03 -0500

    Finished variable visualization plots

--- eda.R
+++ eda.R
@@ -5,31 +5,11 @@
 #--------------------------
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 library(grid)
 library(scales)
 
 load("data/pawns_clean.RData")
-
-
-#-------------------
-# Useful functions
-#-------------------
-theme_ath <- function(base_size=12, base_family="Source Sans Pro Light") {
-  ret <- theme_bw(base_size, base_family) + 
-    theme(panel.background = element_rect(fill="#ffffff", colour=NA),
-          axis.title.x=element_text(vjust=-0.2), axis.title.y=element_text(vjust=1.5),
-          title=element_text(vjust=1.2, family="Source Sans Pro Semibold"),
-          #panel.border = element_blank(), 
-          #panel.grid=element_blank(), 
-          #axis.line=element_blank(),
-          axis.ticks=element_blank(),
-          #legend.position="none", 
-          axis.title=element_text(size=rel(0.8), family="Source Sans Pro Semibold"),
-          strip.text=element_text(size=rel(1), family="Source Sans Pro Semibold"),
-          strip.background=element_rect(fill="#ffffff", colour=NA),
-          panel.margin.y=unit(1.5, "lines"))
-  ret
-}
 
 
 #---------------------
@@ -44,16 +24,17 @@ assn.data <- pawns.data %>%
   count(assn, year) %>% group_by(assn) %>% 
   summarize(assn.n = length(n), assn.mean = mean(n),
             assn.sd = sd(n), assn.se = assn.sd / sqrt(assn.n)) %>%
-  mutate(ci.mult = qt(0.95/2 + 0.5, assn.n),
-         assn.ci = assn.se * ci.mult)
+  mutate(assn.ci = assn.se * qt(0.95/2 + 0.5, assn.n))
 
-ggplot(assn.data, aes(x=assn, y=assn.mean)) + 
+assn.plot <- ggplot(assn.data, aes(x=assn, y=assn.mean, fill=assn)) + 
   geom_bar(stat="identity") + 
   geom_errorbar(aes(ymin=assn.mean - assn.ci, ymax=assn.mean + assn.ci),
                 width=0.3) + 
   labs(x="Freedom of assembly and association (CIRI)", 
        y="Average number of countries per year") + 
-  theme_ath() + theme(panel.grid.major.x=element_blank())
+  scale_fill_manual(values=assn.colours, guide=FALSE) + 
+  theme_clean() + theme(panel.grid.major.x=element_blank())
+assn.plot
 
 
 #------------------------
@@ -62,87 +43,83 @@ ggplot(assn.data, aes(x=assn, y=assn.mean)) +
 
 # Regime type
 #------------
-# UDS (uds_*)
+plot.data <- pawns.data %>%
+  select(assn, polity2) %>% 
+  mutate(assn = factor(assn, levels=rev(levels(assn)))) %>% 
+  na.omit()
+
+regime.plot <- ggplot(plot.data, aes(x=assn, y=polity2, fill=assn)) + 
+  geom_hline(yintercept=c(-5, 5), colour="grey70", size=1) + 
+  geom_violin(colour=NA) + coord_flip() + 
+  labs(x="Freedom of assembly and association (CIRI)",
+       y="Polity IV score") +
+  scale_fill_manual(values=rev(assn.colours), guide=FALSE) + 
+  theme_clean()
+regime.plot
 
 
 # Regime stability
 #-----------------
 # ICRG government stability (icrg_stability)
-icrg.data <- pawns.data %>%
-  select(country, year, icrg_stability) %>% na.omit() %>%
-  group_by(year) %>% 
-  summarize(n = n(),
-            icrg.mean = mean(icrg_stability),
-            icrg.sd = sd(icrg_stability),
-            icrg.se = icrg.sd / sqrt(n)) %>%
-  mutate(ci.mult = qt(0.95/2 + 0.5, n),
-         ci = icrg.se * ci.mult)
+plot.data <- pawns.data %>%
+  select(assn, icrg_stability) %>% 
+  mutate(assn = factor(assn, levels=rev(levels(assn)))) %>% 
+  na.omit()
 
-icrg.data1 <- pawns.data %>%
-  select(country, year, icrg_stability) %>% na.omit()
+icrg.plot <- ggplot(plot.data, aes(x=assn, y=icrg_stability, fill=assn)) + 
+  geom_violin(colour=NA) + coord_flip() + 
+  labs(x="Freedom of assembly and association (CIRI)",
+       y="Government stability (ICRG)") +
+  scale_fill_manual(values=rev(assn.colours), guide=FALSE) + 
+  theme_clean()
 
-ggplot(icrg.data, aes(x=year, y=icrg.mean)) + 
-  geom_pointrange(aes(ymin=icrg.mean - ci, ymax=icrg.mean + ci)) + 
-  labs(x="Year", y="Government stability (ICRG)") +
-  theme_ath()
 
-ggplot(icrg.data1, aes(x=year, y=icrg_stability, group=year)) + geom_violin() + theme_ath()
+# Years in office
+plot.data <- pawns.data %>%
+  select(assn, yrsoffc) %>% 
+  mutate(assn = factor(assn, levels=rev(levels(assn)))) %>% 
+  na.omit()
+
+years.office.plot <- ggplot(plot.data, aes(x=assn, y=yrsoffc, fill=assn)) + 
+  geom_violin(colour=NA) + coord_flip() + 
+  labs(x=NULL,
+       y="Years executive has been in office") +
+  scale_fill_manual(values=rev(assn.colours), guide=FALSE) + 
+  theme_clean()
+
+stability <- arrangeGrob(icrg.plot, years.office.plot, nrow=1)
+stability
 
 
 # Regime competitiveness
 #-----------------------
-ggplot(pawns.data, aes(x=years.since.comp)) + geom_histogram()
-
-
-# Other controls
-#---------------
-
-
-
-#--------------------------
-# Bivariate relationships
-#--------------------------
-# UDS/Polity + assn
 plot.data <- pawns.data %>%
-  select(country, year, assn, uds_mean) %>% na.omit()
-  
-ggplot(plot.data, aes(x=assn, y=uds_mean)) + 
-  geom_violin() + 
-  labs(x="Freedom of assembly and association (CIRI)", y="Mean UDS score") + 
-  theme_ath() + theme(panel.grid.major.x=element_blank())
+  select(assn, years.since.comp) %>% 
+  filter(years.since.comp < 20) %>%
+  mutate(assn = factor(assn, levels=rev(levels(assn)))) %>% 
+  na.omit()
+
+years.comp.plot <- ggplot(plot.data, aes(x=assn, y=years.since.comp, fill=assn)) + 
+  geom_violin(colour=NA) + coord_flip() + 
+  labs(x="Freedom of assembly and association (CIRI)",
+       y="Years since a competitive election") +
+  scale_fill_manual(values=rev(assn.colours), guide=FALSE) + 
+  theme_clean()
 
 
-# icrg + assn
 plot.data <- pawns.data %>%
-  select(country, year, assn, icrg_stability) %>% na.omit()
+  select(assn, opp1vote) %>% 
+  mutate(assn = factor(assn, levels=rev(levels(assn))),
+         opp1vote = opp1vote / 100) %>% 
+  na.omit()
 
-ggplot(plot.data, aes(x=assn, y=icrg_stability)) + 
-  geom_violin() + 
-  labs(x="Freedom of assembly and association (CIRI)", 
-       y="Government stability (ICRG)") + 
-  theme_ath() + theme(panel.grid.major.x=element_blank())
+opp.vote.plot <- ggplot(plot.data, aes(x=assn, y=opp1vote, fill=assn)) + 
+  geom_violin(colour=NA) + coord_flip() + 
+  labs(x=NULL,
+       y="Vote share for largest opposition party") +
+  scale_fill_manual(values=rev(assn.colours), guide=FALSE) + 
+  scale_y_continuous(labels=percent) + 
+  theme_clean()
 
-
-# competitiveness + assn
-plot.data <- pawns.data %>%
-  select(year, country, assn, all.comp) %>% na.omit()
-
-ggplot(plot.data, aes(x=assn))
-
-# competitiveness + uds
-
-
-# icrg + uds
-plot.data <- pawns.data %>%
-  select(year, country, icrg_stability, uds_mean) %>% na.omit()
-
-ggplot(plot.data, aes(x=icrg_stability, y=uds_mean)) + 
-  geom_point(alpha=0.5) + geom_smooth(method="lm") + 
-  labs(x="Government stability (ICRG)", y="Mean UDS score") + 
-  theme_ath()
-
-
-# icrg + competitiveness
-
-
-
+competitiveness <- arrangeGrob(years.comp.plot, opp.vote.plot, nrow=1)
+competitiveness
