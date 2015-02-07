effbecdf4329f0cc4723d3992c161519258f8bd5
Andrew Heiss <andrew@andrewheiss.com>
2015-02-06 19:09:14 -0500

    Final models

--- models.R
+++ models.R
@@ -24,9 +24,8 @@ polity.threshold <- 0
 
 # Simple model using UDS
 model.simple.uds <- clm(assn ~ icrg_stability + yrsoffc + 
-                          years.since.comp + opp1vote, 
-                        data=pawns.data, link="logit", Hess=TRUE, 
-                        subset=(uds_mean < uds.threshold))
+                          years.since.comp + opp1vote + uds_mean, 
+                        data=pawns.data, link="logit", Hess=TRUE)
 summary(model.simple.uds)
 
 newdata <- model.simple.uds$model %>% select(-1)
@@ -40,9 +39,8 @@ sep.plot(fitted.values, actual.char=as.character(model.simple.uds$model[,1]),
 
 # Simple model using polity
 model.simple.polity <- clm(assn ~ icrg_stability + yrsoffc + 
-                             years.since.comp + opp1vote, 
-                           data=pawns.data, link="logit", Hess=TRUE, 
-                           subset=(polity2 < polity.threshold))
+                             years.since.comp + opp1vote + polity2, 
+                           data=pawns.data, link="logit", Hess=TRUE)
 summary(model.simple.polity)
 
 newdata <- model.simple.polity$model %>% select(-1)
@@ -56,11 +54,10 @@ sep.plot(fitted.values, actual.char=as.character(model.simple.polity$model[,1]),
 
 # Model + controls with UDS
 model.big.uds <- clm(assn ~ icrg_stability + yrsoffc + 
-                       years.since.comp + opp1vote + 
+                       years.since.comp + opp1vote + uds_mean + 
                        physint + gdpcap.log + population.log + 
-                       oda.log + globalization, 
-                     data=pawns.data, link="logit", Hess=TRUE, 
-                     subset=(uds_mean < uds.threshold))
+                       oda.log + countngo + globalization, 
+                     data=pawns.data, link="logit", Hess=TRUE)
 summary(model.big.uds)
 
 newdata <- model.big.uds$model %>% select(-1)
@@ -74,11 +71,10 @@ sep.plot(fitted.values, actual.char=as.character(model.big.uds$model[,1]),
 
 # Model + controls with polity
 model.big.polity <- clm(assn ~ icrg_stability + yrsoffc + 
-                          years.since.comp + opp1vote + 
+                          years.since.comp + opp1vote + polity2 + 
                           physint + gdpcap.log + population.log + 
-                          oda.log + globalization, 
-                        data=pawns.data, link="logit", Hess=TRUE, 
-                        subset=(polity2 < polity.threshold))
+                          oda.log + countngo + globalization, 
+                        data=pawns.data, link="logit", Hess=TRUE)
 summary(model.big.polity)
 
 newdata <- model.big.polity$model %>% select(-1)
@@ -91,12 +87,12 @@ sep.plot(fitted.values, actual.char=as.character(model.big.polity$model[,1]),
 
 
 # Model + controls + random effects with UDS
-model.full.uds <- clmm(assn ~ icrg_stability + yrsoffc + 
-                        years.since.comp + opp1vote + 
+model.full.uds <- clmm(assn ~ icrg_stability + yrsoffc +
+                        years.since.comp + opp1vote + uds_mean + 
                         physint + gdpcap.log + population.log + 
-                        oda.log + globalization + (1|year) + (1|country), 
-                      data=pawns.data, link="logit", Hess=TRUE, 
-                      subset=(uds_mean < uds.threshold))
+                        oda.log + countngo + globalization + 
+                         (1|year) + (1|country), 
+                      data=pawns.data, link="logit", Hess=TRUE)
 summary(model.full.uds)
 
 newdata <- model.full.uds$model %>% select(-1) %>%
@@ -110,11 +106,11 @@ sep.plot(fitted.values, actual.char=as.character(model.full.uds$model[,1]),
 
 # Model + controls + random effects with polity
 model.full.polity <- clmm(assn ~ icrg_stability + yrsoffc + 
-                            years.since.comp + opp1vote + 
+                            years.since.comp + opp1vote + polity2 +
                             physint + gdpcap.log + population.log + 
-                            oda.log + globalization + (1|year) + (1|country), 
-                          data=pawns.data, link="logit", Hess=TRUE, 
-                          subset=(polity2 < polity.threshold))
+                            oda.log + countngo + globalization + 
+                            (1|year) + (1|country), 
+                          data=pawns.data, link="logit", Hess=TRUE)
 summary(model.full.polity)
 
 newdata <- model.full.polity$model %>% select(-1) %>%
@@ -134,9 +130,11 @@ model.names <- c("Simple (UDS)", "Simple (Polity)", "Full (UDS)", "Full (Polity)
                  "Full + random effects (UDS)", "Full + random effects (Polity)")
 
 coef.names <- c("Government stability (ICRG)", "Years executive in office", 
-                "Years since competitive election", "Opposition vote share", 
+                "Years since competitive election", "Opposition vote share",
+                "Unified democracy score (mean)", "Polity IV",
                 "Physical integrity rights (CIRI)", "GDP per capita (log)", 
-                "Population (log)", "Development aid (log)", "Globalization")
+                "Population (log)", "Development aid (log)", 
+                "INGO members/volunteers (log)", "Globalization")
 
 # Add cutpoints to the startgazer output
 thetas <- bind_rows(lapply(model.list, FUN=function(x) data.frame(x$Theta)))
@@ -152,9 +150,20 @@ stargazer(model.simple.uds, model.simple.polity, model.big.uds, model.big.polity
 
 
 # Coefficient plot
+model.list <- list(model.simple.polity, model.big.polity, model.full.polity)
+
+model.names <- c("Simple", "Full", "Full + random effects")
+
+coef.names.clean <- c("Government stability (ICRG)", "Years executive in office", 
+                      "Years since competitive election", "Opposition vote share",
+                      "Polity IV",
+                      "Physical integrity rights (CIRI)", "GDP per capita (log)", 
+                      "Population (log)", "Development aid (log)", 
+                      "INGO members/volunteers (log)", "Globalization")
+
 coef.plot.data <- bind_rows(lapply(1:length(model.list), FUN=extract.coef.plot, 
                                    models=model.list, names=model.names)) %>%
-  mutate(IV = factor(as.numeric(IV), labels=coef.names, ordered=TRUE),
+  mutate(IV = factor(as.numeric(IV), labels=coef.names.clean, ordered=TRUE),
          IV = factor(IV, levels=rev(levels(IV))))
 
 ggplot(coef.plot.data, aes(x=IV, y=estimate, colour=model.name)) + 
@@ -169,10 +178,12 @@ newdata <- expand.grid(icrg_stability = mean(pawns.data$icrg_stability, na.rm=TR
                        yrsoffc = mean(pawns.data$yrsoffc, na.rm=TRUE),
                        years.since.comp = c(4, 8, 16),
                        opp1vote = seq(0, 50, by=5),
+                       polity2 = c(-6, 6),
                        physint = mean(pawns.data$physint, na.rm=TRUE),
                        gdpcap.log = mean(pawns.data$gdpcap.log, na.rm=TRUE),
                        population.log = mean(pawns.data$population.log, na.rm=TRUE),
                        oda.log = mean(pawns.data$oda.log, na.rm=TRUE),
+                       countngo = mean(pawns.data$countngo, na.rm=TRUE),
                        globalization = mean(pawns.data$globalization, na.rm=TRUE),
                        year = 0, country = 0)
 
@@ -185,7 +196,9 @@ plot.data.single <- cbind(newdata, pred.mat.polity) %>%
   mutate(years.since.comp = factor(years.since.comp), 
          years.since.comp = factor(years.since.comp, 
                                    labels=paste(levels(
-                                     years.since.comp), "years since election")))
+                                     years.since.comp), "years since election"))) %>%
+  mutate(polity2 = factor(polity2, labels=c("Autocracy", "Democracy")),
+         opp1vote = opp1vote/100)
 
 # Plot
 assn.colours <- c("#8C2318", "#F2C45A", "#88A65E")
@@ -193,39 +206,42 @@ ggplot(plot.data.single, aes(x=opp1vote, y=assn.prob, colour=assn)) +
   geom_line(size=2) + 
   labs(x="Opposition vote share (%)", y="Probability of outcome") + 
   scale_colour_manual(values=assn.colours, name="Freedom of association") + 
-  theme_bw() + facet_wrap(~ years.since.comp)
+  theme_clean() + facet_wrap(~ polity2 + years.since.comp)
 
 
 num.simulations <- 500
-mu <- head(c(model.full.uds$Theta, model.full.uds$beta, unlist(model.full.uds$ST)), -1)
-draw <- cbind(MASS::mvrnorm(num.simulations, mu, vcov(model.full.uds)), 0)
+mu <- c(model.full.polity$Theta, model.full.polity$beta, unlist(model.full.polity$ST))
+draw <- MASS::mvrnorm(num.simulations, mu, vcov(model.full.polity))
 
 sim.predict <- lapply(1:nrow(draw), FUN=ologit.predict,
-                      coefs=draw, newdata=newdata, mod=model.full.uds)
+                      coefs=draw, newdata=newdata, mod=model.full.polity)
 
 plot.data <- newdata %>% cbind(bind_rows(sim.predict)) %>%
   gather(assn, assn.prob, -c(1:ncol(newdata), sim.round)) %>%
   mutate(years.since.comp = factor(years.since.comp), 
          years.since.comp = factor(years.since.comp, 
                                    labels=paste(levels(
-                                     years.since.comp), "years since election")))
+                                     years.since.comp), "years since election"))) %>%
+  mutate(polity2 = factor(polity2, labels=c("Autocracy", "Democracy")),
+         opp1vote = opp1vote/100)
 
 # http://www.colourlovers.com/palette/110225/Vintage_Modern
 assn.colours <- c("#8C2318", "#F2C45A", "#88A65E")
 p <- ggplot(plot.data, aes(x=opp1vote, y=assn.prob, colour=assn)) + 
-  geom_line(aes(group=interaction(sim.round, assn)), alpha=0.03, size=1) + 
+  geom_line(aes(group=interaction(sim.round, assn)), alpha=0.01, size=1) + 
   geom_line(data=plot.data.single, size=2) +
-  labs(x="Opposition vote share (%)", y="Probability of outcome") + 
+  labs(x="Opposition vote share", y="Probability of outcome") + 
   ggtitle(expression(atop("Predicted probabilities of freedom of association restrictions", 
                           atop("500 simulated draws. All variables held at their means."), ""))) +
   scale_colour_manual(values=assn.colours, name="Freedom of association") + 
-  scale_y_continuous(labels=percent) + 
+  scale_y_continuous(labels=percent, breaks=seq(0, 0.8, 0.2)) + 
+  scale_x_continuous(labels=percent) + 
   theme_clean(legend.bottom = TRUE) + 
   theme(legend.key = element_blank()) + 
-  facet_wrap(~ years.since.comp)
+  facet_wrap(~ polity2 + years.since.comp)
 p
 
-ggsave(p, filename="fancy_plot.pdf", width=9, height=6, units="in", device=cairo_pdf)
+ggsave(p, filename="fancy_plot.pdf", width=10, height=7, units="in", device=cairo_pdf)
 
 
 # Stability + yrsoffc
@@ -233,10 +249,12 @@ newdata <- expand.grid(icrg_stability = seq(0, 12, by=0.5),
                        yrsoffc = c(4, 8, 12),
                        years.since.comp = mean(pawns.data$years.since.comp, na.rm=TRUE),
                        opp1vote = mean(pawns.data$opp1vote, na.rm=TRUE),
+                       polity2 = c(-6, 6), 
                        physint = mean(pawns.data$physint, na.rm=TRUE),
                        gdpcap.log = mean(pawns.data$gdpcap.log, na.rm=TRUE),
                        population.log = mean(pawns.data$population.log, na.rm=TRUE),
                        oda.log = mean(pawns.data$oda.log, na.rm=TRUE),
+                       countngo = mean(pawns.data$countngo, na.rm=TRUE),
                        globalization = mean(pawns.data$globalization, na.rm=TRUE),
                        year = 0, country = 0)
 
@@ -249,7 +267,8 @@ plot.data.single <- cbind(newdata, pred.mat.polity) %>%
   mutate(yrsoffc = factor(yrsoffc), 
          yrsoffc = factor(yrsoffc, 
                           labels=paste(levels(
-                            yrsoffc), "years in office")))
+                            yrsoffc), "years in office"))) %>%
+  mutate(polity2 = factor(polity2, labels=c("Autocracy", "Democracy")))
 
 # Plot
 assn.colours <- c("#8C2318", "#F2C45A", "#88A65E")
@@ -257,36 +276,37 @@ ggplot(plot.data.single, aes(x=icrg_stability, y=assn.prob, colour=assn)) +
   geom_line(size=2) + 
   labs(x="Government stability (ICRG)", y="Probability of outcome") + 
   scale_colour_manual(values=assn.colours, name="Freedom of association") + 
-  theme_bw() + facet_wrap(~ yrsoffc)
+  theme_bw() + facet_wrap(~ polity2 + yrsoffc)
 
 
 num.simulations <- 500
-mu <- head(c(model.full.uds$Theta, model.full.uds$beta, unlist(model.full.uds$ST)), -1)
-draw <- cbind(MASS::mvrnorm(num.simulations, mu, vcov(model.full.uds)), 0)
+mu <- c(model.full.polity$Theta, model.full.polity$beta, unlist(model.full.polity$ST))
+draw <- MASS::mvrnorm(num.simulations, mu, vcov(model.full.polity))
 
 sim.predict <- lapply(1:nrow(draw), FUN=ologit.predict,
-                      coefs=draw, newdata=newdata, mod=model.full.uds)
+                      coefs=draw, newdata=newdata, mod=model.full.polity)
 
 plot.data <- newdata %>% cbind(bind_rows(sim.predict)) %>%
   gather(assn, assn.prob, -c(1:ncol(newdata), sim.round)) %>%
   mutate(yrsoffc = factor(yrsoffc), 
          yrsoffc = factor(yrsoffc, 
                           labels=paste(levels(
-                            yrsoffc), "years in office")))
+                            yrsoffc), "years in office"))) %>%
+  mutate(polity2 = factor(polity2, labels=c("Autocracy", "Democracy")))
 
 # http://www.colourlovers.com/palette/110225/Vintage_Modern
 assn.colours <- c("#8C2318", "#F2C45A", "#88A65E")
 p1 <- ggplot(plot.data, aes(x=icrg_stability, y=assn.prob, colour=assn)) + 
-  geom_line(aes(group=interaction(sim.round, assn)), alpha=0.03, size=1) + 
+  geom_line(aes(group=interaction(sim.round, assn)), alpha=0.01, size=1) + 
   geom_line(data=plot.data.single, size=2) +
-  labs(x="Opposition vote share (%)", y="Probability of outcome") + 
+  labs(x="Government stability (ICRG)", y="Probability of outcome") + 
   ggtitle(expression(atop("Predicted probabilities of freedom of association restrictions", 
                           atop("500 simulated draws. All variables held at their means."), ""))) +
   scale_colour_manual(values=assn.colours, name="Freedom of association") + 
-  scale_y_continuous(labels=percent) + 
+  scale_y_continuous(labels=percent, breaks=seq(0, 0.8, 0.2)) + 
   theme_clean(legend.bottom = TRUE) + 
   theme(legend.key = element_blank()) + 
-  facet_wrap(~ yrsoffc)
+  facet_wrap(~ polity2 + yrsoffc)
 p1
 
-ggsave(p1, filename="fancy_plot1.pdf", width=9, height=6, units="in", device=cairo_pdf)
+ggsave(p1, filename="fancy_plot1.pdf", width=10, height=7, units="in", device=cairo_pdf)
